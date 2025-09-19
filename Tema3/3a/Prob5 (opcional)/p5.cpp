0ec04a1f312d370a711ace586651885b34b3fa45
Joan Noguera Quer <jobefet@gmail.com>
2025-09-19 15:52:22 +0200

    Problema opcional a5 resolt

---
+++ Tema3/3a/Prob5 (opcional)/p5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char n,c;
+    int edat;
+
+    cin >> n >> c >> edat;
+
+    cout << "Hola " << n << "." << c << ". Tens " << edat << " anys!" << endl;
+
+    return 0;
+}
